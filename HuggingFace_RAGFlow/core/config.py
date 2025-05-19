f4a75d4707d6611b2c1dc42b456d7e44dff161bc
Hyeon <sam8729@naver.com>
2025-05-19 18:58:04 +0900

    최종본

--- HuggingFace_RAGFlow/core/config.py
+++ HuggingFace_RAGFlow/core/config.py
@@ -5,10 +5,11 @@ from dataclasses import dataclass
 class LLMConfig:
     EMBEDDING_MODEL_NAME: str = "nlpai-lab/KURE-v1"
     COLLECTION_NAME: str = "historiacard_docs"
-    QDRANT_STORE_PATH: str = "./tmp/faiss_db_fixed"
+    FAISS_DB_STORE_PATH: str = "./tmp/faiss_db_fixed"
 
     # Model
-    MODEL_NAME: str = "LGAI-EXAONE/EXAONE-3.5-7.8B-Instruct"
+    # MODEL_NAME: str = "LGAI-EXAONE/EXAONE-3.5-7.8B-Instruct"
+    MODEL_NAME: str = "naver-hyperclovax/HyperCLOVAX-SEED-Vision-Instruct-3B"
     MODEL_TASK: str = "text-generation"
     TEMPERATURE: float = 0.7
     MAX_NEW_TOKENS: int = 1024
